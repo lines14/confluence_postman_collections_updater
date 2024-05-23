b2ff25ff85e01f4c3513748c2656ee9ea65ee3ab
lines14 <a.lipatov@a-i.kz>
2024-05-23 11:53:01 +0500

    update

--- collectionsParser.js
+++ collectionsParser.js
@@ -3,7 +3,7 @@ import Logger from './modules/main/logger.js';
 import DataUtils from './modules/main/dataUtils.js';
 import JSONLoader from './modules/main/JSONLoader.js';
 
-const { Collection, ItemGroup, Item } = postmanCollection;
+const { Collection } = postmanCollection;
 
 const parsedCollections = JSONLoader.inputCollectionNames
   .map((collectionName) => new Collection(JSONLoader[collectionName.replace('.json', '')]));
@@ -15,34 +15,8 @@ const sortedCollection = new Collection({
   },
 });
 
-const getOrCreateFolder = (folders, name) => {
-  let folder = folders.find((f) => f.name === name);
-  if (!folder) {
-    folder = new ItemGroup({ name });
-    sortedCollection.items.add(folder);
-  }
-  return folder;
-};
-
-const processItems = (items) => {
-  items.each((item) => {
-    if (item instanceof Item) {
-      const { path } = item.request.url;
-      Logger.log(`Processing "${item.name}" request path: /${path.join('/')}`);
-      if (path && path.length > 1) {
-        const folderName = path[1].toUpperCase();
-        const folder = getOrCreateFolder(sortedCollection.items.members, folderName);
-        folder.items.add(item);
-      }
-    } else if (item.items) {
-      Logger.log(`Processing folder: ${item.name}`);
-      processItems(item.items);
-    }
-  });
-};
-
 if (originalCollection.items && originalCollection.items.count() > 0) {
-  processItems(originalCollection.items);
+  DataUtils.processItems(sortedCollection, originalCollection.items);
 } else {
   Logger.log('No items found in the original collection!');
 }
--- modules/main/dataUtils.js
+++ modules/main/dataUtils.js
@@ -1,9 +1,40 @@
 import fs from 'fs';
+import postmanCollection from 'postman-collection';
+import Logger from './logger.js';
+
+const { ItemGroup, Item } = postmanCollection;
 
 class DataUtils {
   static saveToJSON(collection) {
     const replacer = (key, value) => (typeof value === 'undefined' ? null : value);
     fs.writeFileSync(`./output/${collection.name}.json`, JSON.stringify(collection, replacer, 4));
+  }
+
+  static getOrCreateFolder(sortedCollection, folders, name) {
+    let folder = folders.find((f) => f.name === name);
+    if (!folder) {
+      folder = new ItemGroup({ name });
+      sortedCollection.items.add(folder);
+    }
+
+    return folder;
+  }
+
+  static processItems(sortedCollection, items) {
+    items.each((item) => {
+      if (item instanceof Item) {
+        const { path } = item.request.url;
+        Logger.log(`Processing "${item.name}" request path: /${path.join('/')}`);
+        if (path && path.length > 1) {
+          const folderName = path[1].toUpperCase();
+          const folder = this.getOrCreateFolder(sortedCollection, sortedCollection.items.members, folderName);
+          folder.items.add(item);
+        }
+      } else if (item.items) {
+        Logger.log(`Processing folder: ${item.name}`);
+        this.processItems(sortedCollection, item.items);
+      }
+    });
   }
 }
 
