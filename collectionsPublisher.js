5ab44f32a89e858964d35288940a4dd4b2b15289
lines14 <tonybass1414@yahoo.com>
2025-03-23 17:14:11 +0500

    fix config

--- collectionsPublisher.js
+++ collectionsPublisher.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-await-in-loop */
 /* eslint no-restricted-syntax: ['off', 'ForInStatement'] */
+import dotenv from 'dotenv';
 import JSONLoader from './modules/main/JSONLoader.js';
 import confluenceAPI from './modules/API/confluenceAPI.js';
+
+dotenv.config({ override: true });
 
 const fileObjects = JSONLoader.outputFileObjects
   .filter((fileObj) => JSONLoader.config.collectionNamesToPublish
     .includes(fileObj.fileName));
 
-const response = await confluenceAPI.getAttachments(JSONLoader.config.confluencePageID);
+const response = await confluenceAPI.getAttachments(process.env.CONFLUENCE_PAGE_ID);
 const attachmentsIDs = fileObjects.map((fileObj) => response.data.results
   .filter((element) => element.title === fileObj.file).pop().id);
 
@@ -17,5 +20,5 @@ for (const attachmentID of attachmentsIDs) {
 }
 
 for (const fileObj of fileObjects) {
-  await confluenceAPI.postJSONAttachment(JSONLoader.config.confluencePageID, fileObj);
+  await confluenceAPI.postJSONAttachment(process.env.CONFLUENCE_PAGE_ID, fileObj);
 }
